3d581b1e8db383b84bf5dc2d951fee5c9856a86b
Matt Filetto <matt.filetto@gmail.com>
2024-02-21 10:44:17 -0800

    Drop DeviceIntelligenceNetworkPrebuilt
    * This we get this from vendor/gms already
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- shiba-vendor.mk
+++ shiba-vendor.mk
@@ -82,7 +82,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
     DevicePersonalizationPrebuiltPixel2023 \
     DiagMon \
     EuiccGoogle \
