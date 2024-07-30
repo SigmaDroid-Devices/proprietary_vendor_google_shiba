d346ef6b83750d1c8e4bc70dcc6b6536f5255560
Matt Filetto <matt.filetto@gmail.com>
2024-07-30 10:17:36 +0000

    Remove packages we already track in vendor/gms

--- shiba-vendor.mk
+++ shiba-vendor.mk
@@ -85,8 +85,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
-    DevicePersonalizationPrebuiltPixel2023 \
     DiagMon \
     EuiccGoogle \
     HardwareInfo \
